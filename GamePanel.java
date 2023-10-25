53793f1a9e83c61479ced3e035626ef99d7587e3
Amidu99 <amidushamika@gmail.com>
2023-10-25 13:13:23 +0530

    finalize GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -259,43 +259,36 @@ public class GamePanel extends JPanel implements ActionListener {
 		@Override
 		public void keyPressed(KeyEvent e) {
 			switch (e.getKeyCode()) {
-
 				case KeyEvent.VK_LEFT:
 					if (direction != 'R') {
 						direction = 'L';
 					}
 					break;
-
 				case KeyEvent.VK_RIGHT:
 					if (direction != 'L') {
 						direction = 'R';
 					}
 					break;
-
 				case KeyEvent.VK_UP:
 					if (direction != 'D') {
 						direction = 'U';
 					}
 					break;
-
 				case KeyEvent.VK_DOWN:
 					if (direction != 'U') {
 						direction = 'D';
 					}
 					break;
-
 				case KeyEvent.VK_ENTER:
 					if (!running) {
 						restartButton.doClick();
 					}
 					break;
-
 				case KeyEvent.VK_P:
 					if (timer.isRunning()) {
 						timer.stop();
 					}
 					break;
-
 				case KeyEvent.VK_R:
 					if (!timer.isRunning()) {
 						timer.start();
